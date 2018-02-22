d7fad3e2fe09ef6e80e0b7b1995f42a428a48764
aditya subramanyam <subramanyam963@gmail.com>
2018-02-22 22:39:30 +0530

    added mounted event handler

--- client/components/header/index.js
+++ client/components/header/index.js
@@ -1,15 +1,17 @@
 import {createVirtualNode, default as loki} from 'loki';
 import styles from './index.scss';
 
+const colors = ['red', 'blue'];
 export default class Header extends loki.Component {
     constructor(...args) {
         super(...args);
-        const colors = ['red', 'blue'];
         const counter = 0;
         this.state = {
             counter,
             color: colors[counter % colors.length]
         };
+    }
+    mounted() {
         setInterval(() => {
             const counter = this.state.counter + 1;
             this.setState({
@@ -18,9 +20,7 @@ export default class Header extends loki.Component {
             });
         }, 1000);
     }
-    handleClick() {
-        // console.log('uyo');
-    }
+    handleClick(){}
     render() {
         return <header class={styles.header} onclick={this.handleClick.bind(this)} style={{
             'font-weight': 'bold',
