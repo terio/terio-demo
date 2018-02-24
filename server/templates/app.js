50a7c8ac8dfb9f0ef03d6e65b223985325891ce0
aditya subramanyam <subramanyam963@gmail.com>
2018-02-24 14:09:32 +0530

    indented stuff

--- server/templates/app.js
+++ server/templates/app.js
@@ -4,11 +4,11 @@ const CSS_TEMPLATE = template(`<link rel="stylesheet" type="text/css" href="<%= 
 const JS_TEMPLATE = template(`<script src="<%= js %>"></script>`);
 export default template(`
     <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8">
-                <%= _.get(assets, 'main.css', []).map((css) => CSS_TEMPLATE({css})).join('') %>
-            </head>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <%= _.get(assets, 'main.css', []).map((css) => CSS_TEMPLATE({css})).join('') %>
+        </head>
         <body>
             <div id="app"><%= renderedAppString %></div>
             <%= _.get(assets, 'vendor.js', []).map((js) => JS_TEMPLATE({js})).join('') %>
